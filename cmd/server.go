d9ba23384e155e75b91e1954006314463b124c37
HyeonJun Yoon <hjyoun0731@gmail.com>
2021-06-16 23:55:25 +0900

    add middleware

--- cmd/server.go
+++ cmd/server.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"time"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/lottogo/api"
 )
 
+type Stats struct {
+	Uptime       time.Time      `json:"uptime"`
+	RequestCount uint64         `json:"requestCount"`
+	Statuses     map[string]int `json:"statuses"`
+}
+
+func NewStats() *Stats {
+	return &Stats{
+		Uptime:   time.Now(),
+		Statuses: map[string]int{},
+	}
+}
+
+func (s *Stats) Middleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		err := next(c)
+
+		s.RequestCount++
+		status := c.Path()
+		s.Statuses[status]++
+		return err
+	}
+}
+
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.Recover())
+	s := NewStats()
+	e.Use(s.Middleware)
 
 	e.GET("/", api.Index)
 	e.GET("/random", api.Random)
